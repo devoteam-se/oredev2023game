bc041759dce01feab5b82f9daa4c4c821d9dae24
Jonas Stenberg <github@stenberg.io>
2023-11-05 16:20:48 +0100

    fix: Add cors middleware

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,11 @@
 import { createServer } from './server';
 import express from 'express';
+import cors from 'cors';
 
 const server = createServer();
 
 const prepend = express();
+prepend.use(cors());
 prepend.use('/api', server);
 
 const port = parseInt(process.env.PORT || '3000');
