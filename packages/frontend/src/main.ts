a2cfe6f951c9394714aaed2a187a89fd96906666
Fredrik Blomqvist <fredrik.m.blomqvist@gmail.com>
2023-11-07 16:10:16 +0100

    fix: email validation regex
    This change updates the email validation regex to better handle whitespace characters and unconventional domain names.

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -33,7 +33,7 @@ elements['hide-instructions'].addEventListener('click', () => {
 elements['play-button'].addEventListener('click', async () => {
   const name = elements['name-input'].value;
   const email = elements['email-input'].value;
-  const emailRegex = /^.+@.+\..+$/;
+  const emailRegex = /(".+"|\S+)@\S+/ui;
 
   if (!name) {
     alert('Name must be provided');
